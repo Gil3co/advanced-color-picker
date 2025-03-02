6aa774501d5b544a5cc66365d319cec03e020132
Gil Cohen <gil3co@gmail.com>
2025-03-02 11:44:25 +0000

    Show/hide schemes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,11 +9,14 @@
     HSLColors,
     HexColor,
     RGBColors,
+    RGBColorSwatch,
+    RGBColorSwatchGray,
   } from '$lib';
   import type { Color } from '$lib/types';
 
   let primaryColor = $state<Color>(initialColor);
-
+  let shouldShowHsl = $state(true);
+  let shouldShowRgb = $state(false);
   colorsStore.subscribe((storeValue) => {
     primaryColor = storeValue.primaryColor;
   });
@@ -25,10 +28,43 @@
     <ColorChip color={colorToString(primaryColor, ColorFormat.HSL)} />
     <HexColor />
   </div>
-
+  <div class="scheme-options">
+    <button
+      onclick={() => {
+        const newValue = !shouldShowHsl;
+        if (!shouldShowRgb && !newValue) return;
+        shouldShowHsl = !shouldShowHsl;
+      }}
+    >
+      <span>HSL</span>
+      {#if shouldShowHsl}
+        <RGBColorSwatch />
+      {:else}
+        <RGBColorSwatchGray />
+      {/if}
+    </button>
+    <button
+      onclick={() => {
+        const newValue = !shouldShowRgb;
+        if (!shouldShowHsl && !newValue) return;
+        shouldShowRgb = !shouldShowRgb;
+      }}
+    >
+      <span>RGB</span>
+      {#if shouldShowRgb}
+        <RGBColorSwatch />
+      {:else}
+        <RGBColorSwatchGray />
+      {/if}
+    </button>
+  </div>
   <div class="color-schemes">
-    <HSLColors />
-    <RGBColors />
+    {#if shouldShowHsl}
+      <HSLColors />
+    {/if}
+    {#if shouldShowRgb}
+      <RGBColors />
+    {/if}
   </div>
 </div>
 
@@ -41,6 +77,7 @@
     display: flex;
     flex-direction: column;
     align-items: center;
+    row-gap: 1rem;
   }
 
   h1 {
@@ -52,7 +89,17 @@
     flex-direction: column;
     align-items: center;
     row-gap: 0.5rem;
-    margin-bottom: 1rem;
+  }
+
+  .scheme-options {
+    display: flex;
+    column-gap: 1rem;
+  }
+
+  button {
+    display: flex;
+    align-items: center;
+    column-gap: 0.25rem;
   }
 
   .color-schemes {
